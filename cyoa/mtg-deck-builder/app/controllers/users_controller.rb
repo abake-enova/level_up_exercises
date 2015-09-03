352926e4f1c7a210b01fabde946b98aabf9ea281
Alex Bake <abake@enova.com>
2015-09-03 12:26:49 -0500

    Add line break.

--- cyoa/mtg-deck-builder/app/controllers/users_controller.rb
+++ cyoa/mtg-deck-builder/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_logged_in, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @decks = @user.decks.paginate(page: params[:page])
