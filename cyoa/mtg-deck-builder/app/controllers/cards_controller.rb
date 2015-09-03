77d9304e7e9f46c1b960a128e2243c62c277b4be
Alex Bake <abake@enova.com>
2015-09-03 11:38:06 -0500

    Extract find_card_from_params_id and use it before_action in cards_controller.

--- cyoa/mtg-deck-builder/app/controllers/cards_controller.rb
+++ cyoa/mtg-deck-builder/app/controllers/cards_controller.rb
@@ -1,14 +1,20 @@
 class CardsController < ApplicationController
+  before_action :find_card_from_params_id, except: :index
+
   def index
     @cards = Card.search(params).paginate(page: params[:page], per_page: 50)
   end
 
   def show
-    @card = Card.find(params[:id])
   end
 
   def show_tooltip
-    @card = Card.find(params[:id])
     render 'cards/show_tooltip', layout: false
+  end
+
+  private
+
+  def find_card_from_params_id
+    @card = Card.find(params[:id])
   end
 end
